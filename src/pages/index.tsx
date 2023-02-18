b5593697babf6370744509461677fb2e88105f30
Lili <villegasgomezlilia@gmail.com>
2023-02-18 14:40:13 +0100

    feat: create home view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import HomeView from '$/containers/Views/Home';
+
 function HomePage(): JSX.Element {
-  return <div>Welcome to Next.js!</div>;
+  return <HomeView />;
 }
 
 export default HomePage;
