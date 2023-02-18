813cdf1b0960efd6d070eb99880752a6ecf5bfb3
Lili <villegasgomezlilia@gmail.com>
2023-02-18 14:39:48 +0100

    chore: add styled components

---
+++ src/pages/_document.tsx
@@ -0,0 +1,54 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import React from 'react';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class AppDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const { styles, ...initialProps } = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render = (): JSX.Element => (
+    <Html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+      <div id="portal-root" />
+    </Html>
+  );
+}
