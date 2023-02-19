201f9961861cb98ad1e6f901c822d24e1451074c
Lili <villegasgomezlilia@gmail.com>
2023-02-19 19:48:17 +0100

    chore: add svg webpack loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,23 @@
+const svgLoader = require('../scripts/webpack/svgLoader.js');
+
+// Override default svg loader behaviour
+function removeSvgLoader(config) {
+  const fileLoaderRule = config.module.rules.find(
+    (rule) => rule.test && rule.test.test('.svg'),
+  );
+  fileLoaderRule.exclude = /\.svg$/;
+}
+
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../src/**/stories.@(ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: (config) => {
+    removeSvgLoader(config);
+    svgLoader(config);
+
+    return config;
+  },
 };
--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+const withImages = require('next-images');
+
+const svgLoader = require('./scripts/webpack/svgLoader.js');
+
+module.exports = withImages({
+  fileExtensions: ['jpg', 'jpeg', 'png'],
+  webpack(config) {
+    svgLoader(config, '/_next');
+    return config;
+  },
+  images: {
+    disableStaticImages: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+});
