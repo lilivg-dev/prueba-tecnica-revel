947ba6acab11aa37921b62694d7ba62ae6fef128
Lili <villegasgomezlilia@gmail.com>
2023-02-19 19:48:45 +0100

    feat: add visuallyHidden mixin

--- src/styles/mixins.ts
+++ src/styles/mixins.ts
@@ -6,3 +6,13 @@ export const resetButtonStyle = css`
   background: transparent;
   border: none;
 `;
+
+export const visuallyHidden = css`
+  position: absolute;
+  width: 0.0625rem;
+  height: 0.0625rem;
+  overflow: hidden;
+  white-space: nowrap;
+  clip: rect(0 0 0 0);
+  clip-path: inset(100%);
+`;
