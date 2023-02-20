ba5e42e5a86a88ae16634546b3561fe4a39e8c0e
Lili <villegasgomezlilia@gmail.com>
2023-02-20 22:22:38 +0100

    fix: fix themes import at _app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import GlobalStyle from '$/styles/global';
-import themes from '$/styles/themes';
+import { themes } from '$/styles/themes';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
 
