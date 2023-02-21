91d82743f45a4265a3623c74a7ad217d007425ce
Lili <villegasgomezlilia@gmail.com>
2023-02-21 15:34:22 +0100

    feat: create media for responsive

---
+++ src/styles/media.ts
@@ -0,0 +1,28 @@
+import { keysOf } from '$/utils/keysOf';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import useMediaQueryMUI from '@mui/material/useMediaQuery';
+
+export type BreakpointSizes = keyof typeof sizes;
+
+export const sizes = {
+  zero: 0,
+  mobileSmall: 320,
+  mobile: 560,
+  tabletPortrait: 767,
+  tabletLandscape: 960,
+  laptop: 1200,
+  desktop: 1800,
+} as const;
+
+const minWidthQuery = (width: number) => `@media (min-width: ${width}px)`;
+
+export const from: { [key in BreakpointSizes]: string } = keysOf(sizes).reduce(
+  (acc, key) => ({
+    ...acc,
+    [key]: minWidthQuery(sizes[key]),
+  }),
+  {} as { [key in BreakpointSizes]: string },
+);
+
+export const useMediaQuery = (query: string): boolean =>
+  useMediaQueryMUI(query, { noSsr: true });
