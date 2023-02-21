cce9f2a84477163f67a6d509b66a46ab73be9530
Lili <villegasgomezlilia@gmail.com>
2023-02-21 15:57:11 +0100

    style: add some more global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,32 +3,48 @@ import { normalize } from 'styled-normalize';
 
 const GlobalStyle = createGlobalStyle`
   ${normalize};
+
   @font-face {
     font-family: "Roboto";
     src: url("/fonts/Roboto-Regular.ttf") format("truetype");
   }
+
   @font-face {
     font-family: "Roboto";
     font-weight: 700;
     src: url("/fonts/Roboto-Bold.ttf") format("truetype");
   }
+
   @font-face {
     font-family: "RobotoCondensed";
     src: url("/fonts/RobotoCondensed-Regular.ttf") format("truetype");
   }
+
   @font-face {
     font-family: "RobotoCondensed";
     font-weight: 700;
     src: url("/fonts/RobotoCondensed-Bold.ttf") format("truetype");
   }
+
   * {
     box-sizing: border-box;
   }
+
   body {
     font-family: "Roboto", sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
+
+  p, h1, h2, h3, h4, h5, h6, ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
   #__next {
     display: flex;
     min-height: 100vh;
