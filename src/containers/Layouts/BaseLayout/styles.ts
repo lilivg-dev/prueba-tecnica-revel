8ab746e526a43718070052740eada767e03e8983
Lili <villegasgomezlilia@gmail.com>
2023-02-21 15:40:16 +0100

    style(containers): add background color to BaseLayout

--- src/containers/Layouts/BaseLayout/styles.ts
+++ src/containers/Layouts/BaseLayout/styles.ts
@@ -5,6 +5,7 @@ export const Layout = styled.div`
   display: flex;
   flex-direction: column;
   height: 100%;
+  background-color: ${({ theme }) => theme.colors.darkGrey};
 `;
 
 export const Main = styled.div`
