d3651341e55482f9ad796b8a44ed8e1f8eff2df0
Lili <villegasgomezlilia@gmail.com>
2023-02-21 20:28:34 +0100

    style(Menu): change html element of menu component

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.nav`
   position: absolute;
   inset-block-start: calc(100% + 1.75rem);
   inset-inline-end: 0;
