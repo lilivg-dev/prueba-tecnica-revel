3546043404e86f17ff7bd41e16bc5781ab7b6ec4
Lili <villegasgomezlilia@gmail.com>
2023-02-22 14:49:01 +0100

    refactor(Avatar): delete unnecessary AvatarContainer

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,14 +1,12 @@
 import UserIcon from '$/assets/icons/user.svg';
 
-import { AvatarContainer, Container, Image } from './styles';
+import { Container, Image } from './styles';
 import { Props } from './types';
 
 export function Avatar({ className, url }: Props) {
   return (
     <Container $hasImage={!!url} className={className}>
-      <AvatarContainer tabIndex={0}>
-        {url ? <Image src={url} alt="" /> : <UserIcon />}
-      </AvatarContainer>
+      {url ? <Image src={url} alt="" /> : <UserIcon />}
     </Container>
   );
 }
--- src/components/Avatar/styles.ts
+++ src/components/Avatar/styles.ts
@@ -3,7 +3,8 @@ import styled, { css } from 'styled-components';
 
 import { StyledProps } from './types';
 
-export const AvatarContainer = styled.div`
+export const Container = styled.div<StyledProps>`
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -12,23 +13,17 @@ export const AvatarContainer = styled.div`
   background-color: ${({ theme }) => addAlpha(theme.colors.white, 0.5)};
   border-radius: 50%;
   outline: 0;
-`;
 
-export const Container = styled.div<StyledProps>`
-  position: relative;
+  ${({ $hasImage }) => css`
+    width: 2.875rem;
+    height: 2.875rem;
+    padding: ${!$hasImage ? '0.5rem' : null};
+  `}
 
   svg {
     width: 100%;
     height: 100%;
   }
-
-  ${({ $hasImage }) => css`
-    ${AvatarContainer} {
-      width: 2.875rem;
-      height: 2.875rem;
-      padding: ${!$hasImage ? '0.5rem' : null};
-    }
-  `}
 `;
 
 export const Image = styled.img`
